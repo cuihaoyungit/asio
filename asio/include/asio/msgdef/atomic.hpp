1e97f41b921f15b59ccbb4d6fc363e125b4e4b14
cuihaoyun <cuihaoyun@dasanshi.com>
2022-12-13 18:41:05 +0800

    1

---
+++ asio/include/asio/msgdef/atomic.hpp
@@ -0,0 +1,25 @@
+//
+// atomic.hpp
+// add by [12/13/2022 cuihaoyun]
+//
+
+#ifndef __ATOMIC_HPP__
+#define __ATOMIC_HPP__
+#include <queue>
+#include <mutex>
+#include <asio/msgdef/message.hpp>
+
+namespace asio {
+
+	typedef struct _AtomicData
+	{
+		std::queue<Message> msg_queue;
+		std::mutex queue_mutex;
+		std::condition_variable condition;
+		bool stop = { false };
+	}AtomicData;
+
+}
+
+
+#endif // __ATOMIC_HPP__
