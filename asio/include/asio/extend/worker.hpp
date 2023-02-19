104e71b39ee3a6052a323fcfe253faab6ede37ab
cuihaoyungit <51316572+cuihaoyungit@users.noreply.github.com>
2023-02-19 17:34:02 +0800

    1

--- asio/include/asio/extend/worker.hpp
+++ asio/include/asio/extend/worker.hpp
@@ -57,7 +57,6 @@ namespace asio {
 		}
 		virtual void Init() {}
 		virtual void Exit() {}
-		virtual void Tick() {}
 		Worker(const Worker&) = delete;
 		Worker& operator = (const Worker&) = delete;
 	private:
