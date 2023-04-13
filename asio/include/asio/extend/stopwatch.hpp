c5a03750cd2790613726af43bdd544a8dcbc31e0
cuihaoyungit <51316572+cuihaoyungit@users.noreply.github.com>
2023-04-13 17:25:40 +0800

    1

---
+++ asio/include/asio/extend/stopwatch.hpp
@@ -0,0 +1,34 @@
+#ifndef __STOPWATCH_HPP__
+#define __STOPWATCH_HPP__
+#include <cstdio>
+#include <ctime>
+
+namespace asio {
+    /**
+      Simple stopwatch for measuring elapsed time.
+    */
+    class Stopwatch
+    {
+    private:
+        clock_t m_start;
+
+    public:
+        Stopwatch() { start(); }
+
+        void start() { m_start = clock(); }
+
+        // ms seconds
+        double getElapsedSeconds()
+        {
+            clock_t now;
+
+            now = clock();
+
+            return (double(now - m_start)) / CLOCKS_PER_SEC;
+        }
+    };
+}
+
+
+
+#endif // __STOPWATCH_HPP__
