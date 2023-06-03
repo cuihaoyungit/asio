e54ea9809f5750c2c1bfa69d116504f2259085a5
cuihaoyungit <51316572+cuihaoyungit@users.noreply.github.com>
2023-06-03 17:12:55 +0800

    1

--- asio/include/asio/extend/singleton.h
+++ asio/include/asio/extend/singleton.h
@@ -76,4 +76,41 @@ private:
 
 
 
+//----------------------------------------------------------------------
+template <typename T> class Singleton
+{
+protected:
+	// TODO: Come up with something better than this!
+	// TODO:
+	// TODO: This super-nasty piece of nastiness was put in for continued
+	// TODO: compatability with MSVC++ and MinGW - the latter apparently
+	// TODO: needs this.
+	static T* ms_Singleton;
+public:
+	Singleton(void)
+	{
+		assert(!ms_Singleton);
+		ms_Singleton = static_cast<T*>(this);
+	}
+	~Singleton(void)
+	{
+		assert(ms_Singleton);  ms_Singleton = 0;
+	}
+	static T& getSingleton(void)
+	{
+		assert(ms_Singleton);  return (*ms_Singleton);
+	}
+	static T* getSingletonPtr(void)
+	{
+		return (ms_Singleton);
+	}
+
+private:
+	Singleton& operator=(const Singleton&) { return this; }
+	Singleton(const Singleton&) {}
+};
+
+//----------------------------------------------------------------------
+
+
 #endif // __Singleton_H__
