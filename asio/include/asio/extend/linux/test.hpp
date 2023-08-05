3d490e4b1409c631daebd18644305ee3ddcd58a9
cuihaoyungit <51316572+cuihaoyungit@users.noreply.github.com>
2023-08-05 21:28:53 +0800

    1

---
+++ asio/include/asio/extend/linux/test.hpp
@@ -0,0 +1,7 @@
+#ifndef __TEST_HPP__
+#define __TEST_HPP__
+
+
+
+
+#endif // __TEST_HPP__
---
+++ asio/include/asio/extend/win32/memoryhelp.hpp
@@ -0,0 +1,63 @@
+#pragma once
+#ifndef __DbgMemLeak_H__
+#define __DbgMemLeak_H__
+
+#if defined(WIN32) && defined(_MSC_VER) &&  defined(_DEBUG)
+#include <stdlib.h>
+#include <crtdbg.h>
+#ifndef WIN32_LEAN_AND_MEAN
+	#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#endif
+
+//////////////////////////////////////////////////////////////////////////
+#if defined(WIN32) && defined(_MSC_VER) &&  defined(_DEBUG)
+#ifndef _CRTDBG_MAP_ALLOC
+	#define _CRTDBG_MAP_ALLOC
+#endif
+#include <stdlib.h>
+#include <crtdbg.h>
+#define DEBUG_NEW new( _NORMAL_BLOCK, __FILE__, __LINE__ )
+#define new DEBUG_NEW
+#endif
+//////////////////////////////////////////////////////////////////////////
+
+class DbgMemLeak
+{
+#if defined(WIN32) && defined(_MSC_VER) &&  defined(_DEBUG)
+	_CrtMemState startMemState;
+	_CrtMemState endMemState;
+#endif
+
+public:
+	explicit DbgMemLeak()
+	{
+#if defined(WIN32) && defined(_MSC_VER) &&  defined(_DEBUG)
+		memset(&startMemState, 0, sizeof(_CrtMemState));
+		memset(&endMemState, 0, sizeof(_CrtMemState));
+		_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+		_CrtMemCheckpoint(&startMemState);
+		// Enable MS Visual C++ debug heap memory leaks dump on exit
+		_CrtSetDbgFlag(_CrtSetDbgFlag(_CRTDBG_REPORT_FLAG) | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	};
+
+	~DbgMemLeak()
+	{
+#if defined(WIN32) && defined(_MSC_VER) &&  defined(_DEBUG)
+		{
+			_CrtMemCheckpoint(&endMemState);
+
+			_CrtMemState diffMemState;
+			_CrtMemDifference(&diffMemState, &startMemState, &endMemState);
+			_CrtMemDumpStatistics(&diffMemState);
+			//{
+			//	int leaksBeforeExit = _CrtDumpMemoryLeaks();
+			//}
+		}
+#endif
+	}
+};
+
+#endif //__DbgMemLeak_H__
