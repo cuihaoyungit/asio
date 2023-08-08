1b0d9c1fe1e367d00ba9b50f36904ceaf6cb6584
cuihaoyungit <51316572+cuihaoyungit@users.noreply.github.com>
2023-08-08 23:20:25 +0800

    1

--- asio/include/asio/extend/server.hpp
+++ asio/include/asio/extend/server.hpp
@@ -256,6 +256,18 @@ namespace asio {
         // single thread run
         void Run() override
         {
+            // multi thread
+		    // Run the I/O service on the requested number of threads
+			/*
+            std::vector<std::thread> v;
+			v.reserve(threads - 1);
+			for (auto i = threads - 1; i > 0; --i)
+				v.emplace_back(
+					[&ioc]
+					{
+						ioc.run();
+					});
+            */
             io_context.run();
         }
 
